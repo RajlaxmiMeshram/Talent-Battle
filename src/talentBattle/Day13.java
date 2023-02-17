5349166a61ae0ee799f5dd71c3a07755ca6912fb
Rajlaxmi Meshram <rajlaxmimeshram024@gmail.com>
2023-02-18 00:42:44 +0530

    commit

---
+++ src/talentBattle/Day13.java
@@ -0,0 +1,20 @@
+package talentBattle;
+
+import java.util.Scanner;
+
+public class Day13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int sum=0;
+		for (int i =1;i<=a;i++)
+		{
+			 sum+=i;
+			
+		}
+		System.out.println(sum);
+	}
+
+}
