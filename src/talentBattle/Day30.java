7153b760315de59e229a3ea7a0086c21e7d60b25
Rajlaxmi Meshram <rajlaxmimeshram024@gmail.com>
2023-03-07 00:25:42 +0530

    commit

--- src/talentBattle/Day30.java
+++ src/talentBattle/Day30.java
@@ -1,9 +1,17 @@
+
+/* Day 30 coding Statement : Write a Program to print Length of the string without using strlen() function*/
+
 package talentBattle;
+
+import java.util.Scanner;
 
 public class Day30 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String s =  sc.nextLine();
+		System.out.println(s.length());
 
 	}
 
