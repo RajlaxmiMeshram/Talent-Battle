e67032ddcf548f9e89a210d7da101b77323209d9
Rajlaxmi Meshram <rajlaxmimeshram024@gmail.com>
2023-03-07 00:13:22 +0530

    commit

---
+++ src/talentBattle/Day27.java
@@ -0,0 +1,17 @@
+/*Day 27 coding Statement : Write a program to find the double of the given number without using arithmetic operator
+Description
+For the given input number calculate the double of it without using arithmetic operator.*/
+package talentBattle;
+
+import java.util.Scanner;
+
+public class Day27 {
+
+	public static void main(String[] args) {
+		 Scanner sc= new Scanner(System.in);
+		 int n = sc.nextInt();
+		 System.out.println(n<<1);
+		
+	}
+
+}
